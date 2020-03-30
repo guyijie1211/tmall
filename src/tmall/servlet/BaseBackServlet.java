a8a7786f96d2a5d404f663c2adccc4a49d388997
YJ1211 <guyijie1211@gmail.com>
2020-03-30 14:29:45 +0800

    3/30

---
+++ src/tmall/servlet/BaseBackServlet.java
@@ -0,0 +1,102 @@
+package tmall.servlet;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import tmall.dao.*;
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+public abstract class BaseBackServlet extends HttpServlet {
+    public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String delete(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String edit(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String update(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String list(HttpServletRequest request, HttpServletResponse response, Page page);
+
+    protected CategoryDAO categoryDAO = new CategoryDAO();
+    protected OrderDAO orderDAO = new OrderDAO();
+    protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+    protected ProductDAO productDAO = new ProductDAO();
+    protected ProductImageDAO productImageDAO = new ProductImageDAO();
+    protected PropertyDAO propertyDAO = new PropertyDAO();
+    protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+    protected ReviewDAO reviewDAO = new ReviewDAO();
+    protected UserDAO userDAO = new UserDAO();
+
+    public void service(HttpServletRequest request,HttpServletResponse response){
+        try{
+            //获取分页信息
+            int start = 0;
+            int count = 5;
+            try{
+                start = Integer.parseInt(request.getParameter("page.start"));
+            }
+            catch(Exception e){
+            }
+
+            try{
+                count = Integer.parseInt(request.getParameter("page.count"));
+            }
+            catch(Exception e){
+
+            }
+
+            Page page = new Page(start,count);
+
+            String method = (String) request.getAttribute("method");
+            Method m = this.getClass().getMethod(method, javax.servlet.http.HttpServletRequest.class,
+                    javax.servlet.http.HttpServletResponse.class,Page.class);
+            String redirect = m.invoke(this,request, response,page).toString();
+
+            if(redirect.startsWith("@"))
+                response.sendRedirect(redirect.substring(1));
+            else if(redirect.startsWith("%"))
+                response.getWriter().print(redirect.substring(1));
+            else
+                request.getRequestDispatcher(redirect).forward(request, response);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public InputStream parseUpload(HttpServletRequest request, Map<String, String> params) {
+        InputStream is =null;
+        try {
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            // 设置上传文件的大小限制为10M
+            factory.setSizeThreshold(1024 * 10240);
+
+            List items = upload.parseRequest(request);
+            Iterator iter = items.iterator();
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+                if (!item.isFormField()) {
+                    // item.getInputStream() 获取上传文件的输入流
+                    is = item.getInputStream();
+                } else {
+                    String paramName = item.getFieldName();
+                    String paramValue = item.getString();
+                    paramValue = new String(paramValue.getBytes("ISO-8859-1"), "UTF-8");
+                    params.put(paramName, paramValue);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return is;
+    }
+}
